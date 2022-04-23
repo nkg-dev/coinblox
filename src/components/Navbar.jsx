551641a8fcf584cbe939f01c3891ee508a9ff490
Neil G <95941773+nkg-dev@users.noreply.github.com>
2022-04-23 15:44:21 -0400

    Adds responsive design; (vertical navbar hidden on small screens)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { Link } from 'react-router-dom';
 
+// destructuring the NavigationMenu component
 const { Item, Root } = NavigationMenu;
 
 const Navbar = () => {
 	const styles = {
 		header: 'flex items-center justify-between',
 		logo: 'm-2 text-3xl font-black uppercase cursor-pointer px-2 py-1 leading-none border-4 border-radix-amber-9 rounded-sm w-fit bg-radix-amber-9 dark:bg-radix-amber-dark-9 dark:hover:bg-radix-amber-dark-10 dark:hover:border-radix-amber-dark-10 dark:text-radix-amber-dark-1 text-radix-amber-1 tracking-wide',
-		root: 'm-2 flex w-fit list-none items-center gap-5 rounded-md text-lg font-semibold text-radix-amber-10 dark:text-radix-amber-dark-10',
-		item: 'rounded-sm py-2 px-4 tracking-wider hover:bg-radix-sand-4 dark:hover:bg-radix-sand-dark-4',
+		root: 'hidden m-2 mr-3 md:flex w-fit list-none items-center gap-5 rounded-sm text-lg font-semibold  text-radix-amber-10 dark:text-radix-amber-dark-10',
+		item: 'rounded-sm py-2 px-4 tracking-wider hover:bg-radix-sand-3 dark:hover:bg-radix-sand-dark-3',
 	};
 	return (
 		<div className={styles.header}>
