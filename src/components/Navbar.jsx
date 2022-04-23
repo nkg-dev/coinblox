4987d8b86e86f0dbd5084681d67ba86d09efe8f1
Neil G <95941773+nkg-dev@users.noreply.github.com>
2022-04-22 21:48:20 -0400

    Tweaks Navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ import icon from '../img/cryptocurrency.png';
 const Navbar = () => {
 	const styles = {
 		menuItem:
-			'mx-6 my-3 flex cursor-pointer items-center gap-3 hover:underline dark:text-radix-amber-dark-11 dark:hover:text-radix-amber-dark-10',
-		logo: 'mx-2 text-2xl font-black uppercase cursor-pointer px-4 py-2 border-4 border-radix-amber-9 rounded-md w-fit dark:text-radix-amber-dark-9 dark:hover:text-radix-amber-dark-10 dark:hover:border-radix-amber-dark-10',
+			'px-5 py-3 flex cursor-pointer items-center gap-3 font-semibold dark:text-radix-amber-dark-11 dark:hover:text-radix-amber-dark-12 dark:hover:bg-radix-sand-dark-4',
+		logo: 'mx-2 text-2xl font-black uppercase cursor-pointer px-4 py-2 border-4 border-radix-amber-9 rounded-md w-fit dark:bg-radix-amber-dark-9 dark:hover:bg-radix-amber-dark-10 dark:hover:border-radix-amber-dark-10',
 		header:
 			'm-3 text-5xl font-black uppercase text-radix-amber-9 px-3 py-2 border-4 border-radix-amber-9 rounded-md w-fit',
 	};
