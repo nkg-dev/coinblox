81cd35ee110877d1c8e66815f12e79bad73e516b
Neil G <95941773+nkg-dev@users.noreply.github.com>
2022-04-23 00:29:40 -0400

    Moves to Radix Navigation Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { Button, Menu, Typography, Avatar, Dropdown } from 'antd';
 import { Link } from 'react-router-dom';
 import {
@@ -12,38 +12,54 @@ import {
 } from '@ant-design/icons';
 import icon from '../img/cryptocurrency.png';
 
+const { Item, Root } = NavigationMenu;
+
 const Navbar = () => {
 	const styles = {
-		menuItem:
-			'px-5 py-3 flex cursor-pointer items-center gap-3 font-semibold dark:text-radix-amber-dark-11 dark:hover:text-radix-amber-dark-12 dark:hover:bg-radix-sand-dark-4',
-		logo: 'mx-2 text-2xl font-black uppercase cursor-pointer px-4 py-2 border-4 border-radix-amber-9 rounded-md w-fit dark:bg-radix-amber-dark-9 dark:hover:bg-radix-amber-dark-10 dark:hover:border-radix-amber-dark-10',
-		header:
-			'm-3 text-5xl font-black uppercase text-radix-amber-9 px-3 py-2 border-4 border-radix-amber-9 rounded-md w-fit',
+		// menuItem:
+		// 	'px-5 py-3 flex cursor-pointer items-center gap-3 font-semibold dark:text-radix-amber-dark-11 dark:hover:text-radix-amber-dark-12 dark:hover:bg-radix-sand-dark-4',
+		logo: 'm-2 text-3xl font-black uppercase cursor-pointer px-2 py-1 leading-none border-4 border-radix-amber-9 rounded-md w-fit bg-radix-amber-9 dark:bg-radix-amber-dark-9 dark:hover:bg-radix-amber-dark-10 dark:hover:border-radix-amber-dark-10 dark:text-radix-amber-dark-1 text-radix-amber-1 tracking-wide',
+		// header:
+		// 	'm-3 text-5xl font-black uppercase text-radix-amber-dark-9 px-3 py-2 border-4 border-radix-amber-dark-9 rounded-md w-fit',
+		item: 'rounded-sm py-2 px-4 tracking-wider hover:bg-radix-sand-4 dark:hover:bg-radix-sand-dark-4',
+		root: 'm-2 flex w-fit list-none items-center gap-5 rounded-md text-lg font-semibold text-radix-amber-10 dark:text-radix-amber-dark-10',
 	};
 	return (
-		<div className="nav-container">
-			<div className="logo-container">
+		<div className="flex items-center justify-between">
+			<div className={styles.logo}>
 				{/* <Avatar src={icon} size="large" /> */}
-				{/* <h1 className={styles.header}>Coinblox</h1> */}
-				<Typography.Title level={2} className={styles.logo}>
-					<Link to="/">Coinblox</Link>
-					{/* <Button className="menu-control-container"></Button> */}
-				</Typography.Title>
-				<Menu theme="Dark">
-					<Menu.Item icon={<HomeOutlined />} className={styles.menuItem}>
-						<Link to="/">Home</Link>
-					</Menu.Item>
-					<Menu.Item icon={<WalletOutlined />} className={styles.menuItem}>
-						<Link to="/crypocurrencies">Crypocurrencies</Link>
-					</Menu.Item>
-					<Menu.Item icon={<FundOutlined />} className={styles.menuItem}>
-						<Link to="/exchanges">Exchanges</Link>
-					</Menu.Item>
-					<Menu.Item icon={<BulbOutlined />} className={styles.menuItem}>
-						<Link to="/news">News</Link>
-					</Menu.Item>
-				</Menu>
+				<Link to="/">Coinblox</Link>
 			</div>
+			<div>
+				<Root className={styles.root}>
+					<Item className={styles.item}>
+						<Link to="/">Home</Link>
+					</Item>
+					<Item className={styles.item}>
+						<Link to="/crypocurrencies">Cryptocurrencies</Link>
+					</Item>
+					<Item className={styles.item}>
+						<Link to="/exchanges">Exchanges</Link>
+					</Item>
+					<Item className={styles.item}>
+						<Link to="/news">News</Link>
+					</Item>
+				</Root>
+			</div>
+			{/* <Menu theme="Dark">
+				<Menu.Item icon={<HomeOutlined />} className={styles.menuItem}>
+					<Link to="/">Home</Link>
+				</Menu.Item>
+				<Menu.Item icon={<WalletOutlined />} className={styles.menuItem}>
+					<Link to="/crypocurrencies">Crypocurrencies</Link>
+				</Menu.Item>
+				<Menu.Item icon={<FundOutlined />} className={styles.menuItem}>
+					<Link to="/exchanges">Exchanges</Link>
+				</Menu.Item>
+				<Menu.Item icon={<BulbOutlined />} className={styles.menuItem}>
+					<Link to="/news">News</Link>
+				</Menu.Item>
+			</Menu> */}
 		</div>
 	);
 };
