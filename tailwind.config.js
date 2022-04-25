79a0b93a93145bdd0f1fa30d09a72cb720803cb0
Neil G <95941773+nkg-dev@users.noreply.github.com>
2022-04-24 20:15:52 -0400

    swtiches to Orbitron font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Outfit', ...defaultTheme.fontFamily.sans],
+				sans: ['Orbitron', ...defaultTheme.fontFamily.sans],
 			},
 			colors: {
 				// Step & Use Case for Radix UI Colors
